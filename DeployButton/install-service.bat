aee9c9a68ae63425d32f17e48cf8dbd69be0a8e0
khlebnikovroman <khlebnikov.roman187@gmail.com>
2025-10-18 22:34:56 +0300

    add .bat

---
+++ DeployButton/install-service.bat
@@ -0,0 +1,14 @@
+﻿@echo off
+cd /d "%~dp0"
+
+sc create DeployButtonService ^
+    binPath= "%~dp0DeployButtonService.exe" ^
+    start= auto ^
+    DisplayName= "Deploy Button Service"
+
+sc description DeployButtonService "Запускает деплой по сигналу с Arduino"
+
+net start DeployButtonService
+
+echo Служба установлена и запущена.
+pause
---
+++ DeployButton/uninstall-service.bat
@@ -0,0 +1,8 @@
+﻿@echo off
+cd /d "%~dp0"
+
+net stop DeployButtonService
+sc delete DeployButtonService
+
+echo Служба удалена.
+pause
