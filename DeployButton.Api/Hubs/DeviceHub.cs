62464bf5c70e8f98895b360be9014401ceec59b1
khlebnikovroman <khlebnikov.roman187@gmail.com>
2025-11-26 01:58:18 +0300

    animation;

---
+++ DeployButton.Api/Hubs/DeviceHub.cs
@@ -0,0 +1,31 @@
+﻿using DeployButton.Api.Abstractions;
+using DeployButton.Api.Configs;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Options;
+
+namespace DeployButton.Api.Hubs;
+
+public class DeviceHub : Hub
+{
+    private readonly IDeviceStateProvider _deviceStateProvider;
+    private readonly IOptionsMonitor<AppSettings> _options;
+
+    public DeviceHub(
+        IDeviceStateProvider deviceStateProvider,
+        IOptionsMonitor<AppSettings> options)
+    {
+        _deviceStateProvider = deviceStateProvider;
+        _options = options;
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        // Отправляем текущее состояние устройства
+        await Clients.Caller.SendAsync("DeviceStateChanged", _deviceStateProvider.CurrentState);
+
+        // Отправляем текущую конфигурацию (опционально)
+        await Clients.Caller.SendAsync("ConfigUpdated", _options.CurrentValue);
+
+        await base.OnConnectedAsync();
+    }
+}
